e472154b2d33885b28a8d805c0e228b38a3cce2c
V4Victini <h.f.gordonjr@gmail.com>
2014-07-24 01:20:51 -0400

    Move `loadDropdowns()` to `Display`
    Also add handlers for '.set-select' elements

--- init.js
+++ init.js
@@ -5,129 +5,14 @@ $p2 = $( "#p2-pokemon" );
 // $p1 = $( "#p1-pokemon" );
 
 // Initialize Dropdowns
-(function loadDropdowns() {
-	$dropdowns = $( "select" );
-	// console.log($dropdowns);
-	for (var index = 0; index < $dropdowns.length; index++) {
-		$dropdown = $dropdowns.eq(index);
-		// if ($dropdown.children().length) continue;
-		var options = []; // Option Elements to be Appended
-		var customs = []; // Custom Option Elements
-		if ($dropdown.hasClass("type-select")) {
-			for (var type in Data.BattleTypeChart) options.push('<option value="'+type+'">'+type+'</option>');
-			$dropdown.append(options.join(''));
-			continue;
-		}
-		if ($dropdown.hasClass("move-select")) {
-			var customMoves = [];
-			for (var id in Data.BattleMovedex) {
-				var move = Data.BattleMovedex[id];
-				if (id !== move.id) continue;
-				if (move.isNonstandard) {
-					customMoves.push(id);
-					continue;
-				}
-				options.push('<option value="'+move.id+'">'+move.name+'</option>');
-			}
-			options.push('<optgroup label="Custom Moves">');
-			for (var i = 0; i < customMoves.length; i++) {
-				var id = customMoves[i];
-				var move = Data.BattleMovedex[id];
-				options.push('<option value="'+id+'">'+move.name+'</option>');
-			}
-			options.push('</optgroup>');
-			$dropdown.append(options.join(''));
-			continue;
-		}
-		if ($dropdown.hasClass("cat-select")) {
-			options = [
-				'<option value="Physical">Physical</option>',
-				'<option value="Special">Special</option>',
-				'<option value="Status">Status</option>'
-			];
-			$dropdown.append(options.join(''));
-			continue;
-		}
-		if ($dropdown.hasClass("ability-select")) {
-			for (var id in Data.BattleAbilities) {
-				var ability = Data.BattleAbilities[id];
-				if (id !== ability.id) continue;
-				options.push('<option value="'+ability.id+'">'+(ability.isNonstandard?'Custom: ':'')+ability.name+'</option>');
-			}
-			$dropdown.append(options.join(''));
-			continue;
-		}
-		if ($dropdown.hasClass("item-select")) {
-			for (var id in Data.BattleItems) {
-				var item = Data.BattleItems[id];
-				if (id !== item.id) continue;
-				options.push('<option value="'+item.id+'">'+(item.isNonstandard?'Custom: ':'')+item.name+'</option>');
-			}
-			$dropdown.append(options.join(''));
-			continue;
-		}
-		if ($dropdown.hasClass("nature-select")) {
-			for (var id in Data.Natures) {
-				var nature = Data.Natures[id];
-				options.push('<option value="'+id+'">'+nature.name+'</option>');
-			}
-			$dropdown.append(options.join(''));
-			continue;
-		}
-		if ($dropdown.hasClass("status-select")) {
-			for (var id in Data.Statuses) {
-				var status = Data.Statuses[id];
-				options.push('<option value="'+id+'">'+status.name+'</option>');
-			}
-			$dropdown.append(options.join(''));
-			continue;
-		}
-		if ($dropdown.hasClass("gender-select")) {
-			options = [
-				'<option value="M">'+Display.genderSymbols['m']+'</option>',
-				'<option value="F">'+Display.genderSymbols['f']+'</option>',
-				'<option value="N">'+Display.genderSymbols['n']+'</option>'
-			];
-			$dropdown.append(options.join(''));
-			continue;
-		}
-		if ($dropdown.hasClass("pkm-select")) {
-			// Pokemon are placed in alphabetical order for ease of location.
-			var Pokedex = Data.BattlePokedex;
-			var ids = Object.keys(Pokedex).sort();
-			var customPkms = [];
-			for (var i = 0; i < ids.length; i++) {
-				var id = ids[i];
-				var pkm = Data.BattlePokedex[id];
-				if (!pkm.num || pkm.num < 1) {
-					customPkms.push(id);
-					continue;
-				}
-				options.push('<option value="'+id+'">'+pkm.species+'</option>');
-			}
-			options.push('<optgroup label="Custom Pokemon">');
-			for (var i = 0; i < customPkms.length; i++) {
-				var id = customPkms[i];
-				var pkm = Data.BattlePokedex[id];
-				// Consider <optgroup label="Custom Pokemon"></optgroup>
-				options.push('<option value="'+id+'">'+pkm.species+'</option>');
-			}
-			options.push('</optgroup>');
-			$dropdown.append(options.join(''));
-			continue;
-		}
-	}
-})();
+Display.loadDropdowns();
 
 // Add Handlers
 // Species Handler
 (function addHandlers() {
 	$('.pkm-select').change(
 		function () {
-			$this = $(this);
-			if (!$this.val()) return Display.clearAllFields($this.parent());
-			Display.showPokemon($this.parent(), new Pokemon($this.val(), (Data.Sets[$this.val()] ? Data.Sets[$this.val()].sets[0] : null)));
-			Display.updateCalcs();
+			Display.changePokemon($(this));
 		}
 	);
 	var elements = [
@@ -141,8 +26,8 @@ $p2 = $( "#p2-pokemon" );
 	]
 	$(elements.join(', ')).change(
 		function () {
-			$this = $(this);
-			Display.updatePokemon($this.parents('.pokemon-pane'));
+			// $this = $(this);
+			Display.updatePokemon($(this).parents('.pokemon-pane'));
 		}
 	);
 
@@ -155,25 +40,18 @@ $p2 = $( "#p2-pokemon" );
 		}
 	);
 
-	/*/ Stat Handlers
-	// Terribly inefficient but this'll change later anyway lol
-	$('.ev-input, .iv-input').change(
+	// Set Handler
+	$('.set-select').change(
 		function () {
 			$this = $(this);
-			if (!$this.val()) return;
-			Display.showPokemon($this.parents('.pokemon-pane'), Display.getPokemon($this.parents('.pokemon-pane')));
+			var val = $this.val();
+			if (val === "") return Display.clearAllFields();
+			var $side = $this.parent();
+			pkm = Display.getPokemon($side);
+			if (val === "R") return; // Do stuff here later
+			set = Data.getSets(pkm.id)[val];
+			Display.showPokemon($side, Display.getPokemon($side).changeSet(set));
 		}
-	);*/
+	);
 })()
 
-// Bind event listeners
-/*
-function bindEvents($side) {
-	if (!$side || $side !instanceof jQuery) return null;
-	if (!$side.hasClass("pokemon-pane")) return false;
-	$side.children("");
-}
-*/
-// First P1
-// Then P2
-// Finally Misc Input
